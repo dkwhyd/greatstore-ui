8a24272e39e91c17003096e67e335c8e25d6029d
dkwhyd <dikiwhyd99@gmail.com>
2024-01-11 22:23:40 +0700

    add list menu

---
+++ src/menu.jsx
@@ -0,0 +1,16 @@
+import { FaHome } from "react-icons/fa";
+import { FaPizzaSlice } from "react-icons/fa";
+const menus = [
+  {
+    icon: <FaHome />,
+    name: "Semua",
+    id: "semua",
+  },
+  {
+    icon: <FaPizzaSlice />,
+    name: "Utama",
+    id: "utama",
+  },
+];
+
+export default menus;
