fd2c71d5721d0ce1492dd4912ad3a8144f9f04b3
dkwhyd <dikiwhyd99@gmail.com>
2024-01-11 23:24:14 +0700

    add search

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
-import axios, { Axios } from "axios";
+import { MdAddShoppingCart } from "react-icons/md";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function Product() {
@@ -18,15 +19,29 @@ export default function Product() {
 
   return (
     <div className="">
-      <h3> menu</h3>
-      <div className="mt-2">
+      <div className="flex flex-row justify-evenly pt-2">
+        <input type="text" className="w-5/6" />
+        <button className="bg-red-200 p-2">Search</button>
+      </div>
+      <div className="mt-2 flex flex-row  flex-wrap">
         {products &&
           products.map((product, index) => {
             return (
-              <div key={index}>
-                <h4>{product.name}</h4>
-                <img src="" alt="" />
+              <div
+                key={index}
+                className="flex flex-col w-32 h-auto bg-red-500 p-2  m-2"
+              >
+                <img
+                  src={`http://localhost:3000/upload/${product.image_url}`}
+                  alt=""
+                  className="min-h-10 w-48 drop-shadow-lg"
+                />
+                <h4>{product.name} </h4>
+
                 <p>Rp.{product.price}</p>
+                <button className="h-10 w-5 p-1  bg-gray-200">
+                  <MdAddShoppingCart />
+                </button>
               </div>
             );
           })}
